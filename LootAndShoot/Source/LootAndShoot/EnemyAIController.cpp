48ea99edcdec2e00268297404b654bb14ef54621
JM Jeon <jmjeon911@gmail.com>
2022-04-03 00:30:17 +0900

    Enemy AI 간단 세팅

---
+++ LootAndShoot/Source/LootAndShoot/EnemyAIController.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyAIController.h"
+
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+#include "NavigationSystem.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+
+AEnemyAIController::AEnemyAIController()
+{
+	static ConstructorHelpers::FObjectFinder<UBehaviorTree> BT(TEXT("BehaviorTree'/Game/_LootAndShoot/AI/BT_Enemy.BT_Enemy'"));
+	if (BT.Succeeded())
+	{
+		BehaviorTree = BT.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UBlackboardData> BB(TEXT("BlackboardData'/Game/_LootAndShoot/AI/BB_Enemy.BB_Enemy'"));
+	if (BB.Succeeded())
+	{
+		BlackboardData = BB.Object;
+	}
+}
+
+void AEnemyAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &AEnemyAIController::RandomMove, 2.f, true);
+
+	if (UseBlackboard(BlackboardData, Blackboard))
+	{
+		if (RunBehaviorTree(BehaviorTree))
+		{
+			// TODO
+		}
+	}
+}
+
+void AEnemyAIController::OnUnPossess()
+{
+	Super::OnUnPossess();
+
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+}
+
+void AEnemyAIController::RandomMove()
+{
+	auto CurrentPawn = GetPawn();
+
+	UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetNavigationSystem(GetWorld());
+	if (NavSystem == nullptr)
+		return;
+
+	FNavLocation RandomLocation;
+	if (NavSystem->GetRandomPointInNavigableRadius(FVector::ZeroVector, 500.f, RandomLocation))
+	{
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, RandomLocation);
+	}
+}
---
+++ LootAndShoot/Source/LootAndShoot/EnemyAIController.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOOTANDSHOOT_API AEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AEnemyAIController();
+
+protected:
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void OnUnPossess() override;
+
+	// 테스트용
+	void RandomMove();
+
+private:
+	FTimerHandle TimerHandle;
+
+	UPROPERTY()
+	class UBehaviorTree* BehaviorTree;
+
+	UPROPERTY()
+	class UBlackboardData* BlackboardData;
+
+};
